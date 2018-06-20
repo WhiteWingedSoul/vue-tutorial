a18bbeb874799548f81871eda21959446b8f868e
White <cooro1994@gmail.com>
2018-06-20 18:57:26 +0900

    second tutorial

---
+++ src/App2.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+
+    <!-- navigation bar -->
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <a class="navbar-brand"><i class="glyphicon glyphicon-bullhorn"></i> Vue Events Bulletin Board</a>
+      </div>
+    </nav>
+
+    <!-- main body of our application -->
+    <Events class="container"/>
+
+  </div>
+</template>
+
+<script>
+import Events from './components/Events'
+
+export default {
+  name: 'App2',
+  components: {
+    Events
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+.container {
+  padding-top: 20px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import App2 from './App2'
 
 Vue.config.productionTip = false
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
-  components: { App },
-  template: '<App/>',
+  components: { App2 },
+  template: '<App2/>',
 })
 
-require('bootstrap/dist/css/bootstrap.css')
+require('../node_modules/bootstrap/dist/css/bootstrap.min.css')
