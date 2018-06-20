517a05e78308aa06027aa9fe170448af9127bdd4
White <cooro1994@gmail.com>
2018-06-20 16:04:16 +0900

    i don't need that

--- src/main.js
+++ src/main.js
@@ -2,11 +2,8 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import BoostrapVue from 'bootstrap-vue'
 
 Vue.config.productionTip = false
-Vue.use(BoostrapVue)
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
